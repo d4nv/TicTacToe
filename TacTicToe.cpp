bd49c207a47332cf1337e182b1e92a8528e9ded8
Emre Demir <g00427391@atu.ie>
2024-03-14 11:48:13 +0000

    PC Won loop fixed

--- TacTicToe.cpp
+++ TacTicToe.cpp
@@ -14,6 +14,20 @@ int main()
 	char symbol;
 	char symbolpc;
 	int r2;
+	int winner;
+
+		int youwin[8][3] = {
+    	{1, 2, 3}, 
+   		{4, 5, 6}, 
+    	{7, 8, 9}, 
+    	{1, 4, 7}, 
+    	{2, 5, 8}, 
+    	{3, 6, 9}, 
+    	{1, 5, 9}, 
+    	{3, 5, 7}  
+	};
+
+	int counter;
 	
 
 	printf("--------------------------\n");
@@ -35,28 +49,6 @@ int main()
 		printf("Please enter the symbol you want to play(X or O): \n");
 		scanf_s(" %c", &symbol);
 
-		int pcnum = rand() % 9 + 1;
-
-		for(int c = 1 ; c <= 9; c++)
-		{
-			if(space[pcnum] == 'X' || space[pcnum] == 'O')
-			{
-				pcnum = rand() % 9 + 1;
-			}
-		}
-		
-
-		int r2 = rand() % 2;
-
-		if(r2==1)
-		{
-			symbolpc = 'X';
-		}
-		else if(r2 == 0)
-		{
-			symbolpc = 'O';
-		}
-
 		char x = 'X';
 		char o = 'O';
 
@@ -70,6 +62,77 @@ int main()
 			space[num] = 'O';
 		}
 
+
+
+	for (counter = 0; counter < 8; counter++)
+ 	{
+   	 if (	space[youwin[counter][0]] == 'X' &&
+    	    space[youwin[counter][1]] == 'X' &&
+    	    space[youwin[counter][2]] == 'X') 
+		{
+			printf("========\n");
+       	 	printf("You won!\n");
+			printf("========\n\n\n");
+
+			printf(" %c | %c | %c \n", space[1], space[2], space[3]);
+			printf("_________\n");
+			printf(" %c | %c | %c \n", space[4], space[5], space[6]);
+			printf("_________\n");
+			printf(" %c | %c | %c \n\n", space[7], space[8], space[9]);	
+
+			i = 20;
+        	
+    	}
+
+		else if (	space[youwin[counter][0]] == 'O' &&
+    	    space[youwin[counter][1]] == 'O' &&
+    	    space[youwin[counter][2]] == 'O') 
+		{
+			printf("========\n");
+       	 	printf("You won!\n");
+			printf("========\n\n\n");
+
+			printf(" %c | %c | %c \n", space[1], space[2], space[3]);
+			printf("_________\n");
+			printf(" %c | %c | %c \n", space[4], space[5], space[6]);
+			printf("_________\n");
+			printf(" %c | %c | %c \n\n", space[7], space[8], space[9]);	
+
+			
+
+			i = 20;
+
+    	}
+	}
+
+		if(i == 20)
+		{
+			break;
+		}
+	
+
+		int pcnum = rand() % 9 + 1;
+
+		for(int c = 1 ; c <= 9; c++)
+		{
+			if(space[pcnum] == 'X' || space[pcnum] == 'O')
+			{
+				pcnum = rand() % 9 + 1;
+			}
+		}
+		
+
+		int r2 = rand() % 500;
+
+		if(r2 % 2 == 1)
+		{
+			symbolpc = 'X';
+		}
+		else if(r2 % 2 == 0)
+		{
+			symbolpc = 'O';
+		}
+
 		
 		if (symbolpc == 'X' || symbolpc == 'x')
 		{
@@ -81,29 +144,11 @@ int main()
 			space[pcnum] = 'O';
 		}
 
-	int youwin[8][3] = {
-    	{1, 2, 3}, 
-   		{4, 5, 6}, 
-    	{7, 8, 9}, 
-    	{1, 4, 7}, 
-    	{2, 5, 8}, 
-    	{3, 6, 9}, 
-    	{1, 5, 9}, 
-    	{3, 5, 7}  
-	};
-
-	int counter;
-		
 
 
-		printf(" %c | %c | %c \n", space[1], space[2], space[3]);
-		printf("_________\n");
-		printf(" %c | %c | %c \n", space[4], space[5], space[6]);
-		printf("_________\n");
-		printf(" %c | %c | %c \n\n", space[7], space[8], space[9]);	
+	
 
-		if( i % 2 == 0)
-	{
+	
 	for (counter = 0; counter < 8; counter++)
  	{
    	 if (	space[youwin[counter][0]] == 'X' &&
@@ -111,38 +156,17 @@ int main()
     	    space[youwin[counter][2]] == 'X') 
 		{
 			printf("========\n");
-       	 	printf("You won!\n");
-			printf("========\n\n\n");
-
-        	i = 20;
-    	}
-
-		else if (	space[youwin[counter][0]] == 'O' &&
-    	    space[youwin[counter][1]] == 'O' &&
-    	    space[youwin[counter][2]] == 'O') 
-		{
-			printf("========\n");
-       	 	printf("You won!\n");
-			printf("========\n\n\n");
-
-        	i = 20;
-    	}
-	}
-	}
-
-	if( i % 2 == 1)
-	{
-	for (counter = 0; counter < 8; counter++)
- 	{
-   	 if (	space[youwin[counter][0]] == 'O' &&
-    	    space[youwin[counter][1]] == 'O' &&
-    	    space[youwin[counter][2]] == 'O') 
-		{
-			printf("========\n");
        	 	printf("PC won!\n");
 			printf("========\n\n\n");
 
-        	i = 20;
+			printf(" %c | %c | %c \n", space[1], space[2], space[3]);
+			printf("_________\n");
+			printf(" %c | %c | %c \n", space[4], space[5], space[6]);
+			printf("_________\n");
+			printf(" %c | %c | %c \n\n", space[7], space[8], space[9]);	
+
+
+			i = 20;
     	}
 
 		else if (	space[youwin[counter][0]] == 'O' &&
@@ -153,12 +177,35 @@ int main()
        	 	printf("PC won!\n");
 			printf("========\n\n\n");
 
+
+			printf(" %c | %c | %c \n", space[1], space[2], space[3]);
+			printf("_________\n");
+			printf(" %c | %c | %c \n", space[4], space[5], space[6]);
+			printf("_________\n");
+			printf(" %c | %c | %c \n\n", space[7], space[8], space[9]);	
+
+
         	i = 20;
     	}
 	}
-	}
+
+		if(i == 20)
+		{
+			break;
+		}
+	
+
+	
+
+		printf(" %c | %c | %c \n", space[1], space[2], space[3]);
+		printf("_________\n");
+		printf(" %c | %c | %c \n", space[4], space[5], space[6]);
+		printf("_________\n");
+		printf(" %c | %c | %c \n\n", space[7], space[8], space[9]);	
 			
 }
+
+	
 
 return 0;
 }
